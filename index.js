0ee7d9c209364687f93ebcfaaedb1ecf692e7fbe
Bo Brockmann <bbrockmann1@gmail.com>
2022-07-13 14:44:59 -0600

    commit tree 2d22ee75e165653f7edc79331f3f822340c09db7

---
+++ index.js
@@ -0,0 +1,37 @@
+function add (num1, num2) {
+    return num1 + num2 
+}
+
+add (83, 83)
+function subtract (num1, num2) {
+    return num1 - num2
+}
+
+subtract (0, 42)
+
+function multiply (num1, num2) {
+    return num1 * num2
+}
+
+multiply (1, 328532)
+
+function divide (num1, num2) {
+    return num1 / num2
+}
+
+divide (3241, 4243.32561307)
+
+function increment (num) {
+    return num + 1
+}
+
+function decrement (num) {
+    return num - 1
+}
+
+function makeInt (string) {
+    return parseInt (string, 10)
+}
+function preserveDecimal(string){
+    return parseFloat(string,10)
+}
